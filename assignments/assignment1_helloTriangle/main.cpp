b5a44bacdf04d0e62b75e8832b7ee7d5767b6d6c
brandon.salvietti <brandon.salvietti@mymail.champlain.edu>
2023-09-09 16:07:00 -0400

    finished assignment

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -8,36 +8,45 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
-float vertices[9] = {
-	//x   //y  //z
-	-0.5, -0.5, 0.0, //Bottom left
-	 0.5, -0.5, 0.0, //Bottom right
-	 0.0,  0.5, 0.0  //Top center
+float vertices[21] = {
+	//x   //y  //z   //r  //g  //b  //a
+	-0.5, -0.5, 0.0, 1.0, 0.7, 0.0, 1.0, //Bottom left
+	 0.5, -0.5, 0.0, 0.7, 1.0, 0.0, 1.0, //Bottom right
+	 0.0,  0.5, 0.0, 0.7, 0.0, 1.0, 1.0  //Top center
 };
+
 
 const char* vertexShaderSRC = R"(
 	#version 450
 	layout(location = 0) in vec3 vPos;
+	layout(location = 1) in vec4 vColor;
+	out vec4 Color;
+	uniform float _Time;
 	void main() {
-		gl_Position = vec4(vPos,1.0);
+		Color = vColor;
+		vec3 offset = vec3(0,sin(vPos.x + _Time),0)*0.5;
+		gl_Position = vec4(vPos + offset,1.0);
 	}
 )";
 const char* fragmentShaderSRC = R"(
 	#version 450
 	out vec4 FragColor;
+	in vec4 Color;
+	uniform float _Time;
 	void main(){
-		FragColor = vec4(1.0);
+		FragColor = Color * abs(sin(_Time));
 	}
 )";
 
 
 unsigned int createVAO(float* vertexData, int numVertices)
 {
+	int stride = 7;
 	unsigned int vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	//Allocate space for + send vertex data to GPU.
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * numVertices, vertexData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * stride * numVertices, vertexData, GL_STATIC_DRAW);
 
 	unsigned int vao;
 	glGenVertexArrays(1, &vao);
@@ -46,8 +55,13 @@ unsigned int createVAO(float* vertexData, int numVertices)
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
 	//Define position attribute (3 floats)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	// first paramter is the location int, second one is the amount of data being sent through, third one is the size allocation
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (const void*)0);
 	glEnableVertexAttribArray(0);
+
+	//Define color attribute (4 floats)
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (const void*)(sizeof(float)*3));
+	glEnableVertexAttribArray(1);
 	return vao;
 };
 
@@ -110,16 +124,32 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
-	
+	// Create shader program and vertex buffer array
 	unsigned int shader = createShaderProgram(vertexShaderSRC, fragmentShaderSRC);
 	unsigned int vao = createVAO(vertices, 3);
 
+	// Render Loop
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shader);
 		glBindVertexArray(vao);
+
+		// Time value update
+		float time = (float)glfwGetTime();
+
+		// get uniform location inside the shader program
+		// as long as it references the correct name, i.e "_Time" in this case
+		// the vertex and fragment shaders will see this value
+		// and will be able to both read from it, as long as it is
+		// denoted using "uniform typename _UniformName"
+
+		int timeLocation = glGetUniformLocation(shader, "_Time");
+		
+		// set uniform value at the uniform location
+		glUniform1f(timeLocation, time);
+
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 	}
